36165a0cbf176acd69d54e6da65b383293fc9727
ping813 <pinglovemusic@163.com>
2018-05-29 17:52:11 +0800

    log--首页友情链接样式修改 (#19)

--- ui/link/met_11_1/index.php
+++ ui/link/met_11_1/index.php
@@ -6,7 +6,7 @@
         <h2>{$ui.footlink_title}</h2>
         <ul class="breadcrumb p-0 link-img m-0">
             <tag action='link.list'>
-            <li class="col-lg-3 col-md-3 col-xs-6">
+            <li class="col-lg-1 col-md-3 col-xs-6">
                 <a href="{$v.weburl}" title="{$v.webname}" target="_blank">
                 <if value="$v.link_type eq 1">
                     <img data-original="{$v.weblogo}" alt="{$v.webname}" height='40'>
